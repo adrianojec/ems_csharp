c39afac0a4d103ec7bca1f7f137125ac0e4c800a
adrianojec <adrianojeric.28@gmail.com>
2022-10-18 19:45:49 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ems_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
