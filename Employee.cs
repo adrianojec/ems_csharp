13a14b3fb7b2d010464fdfa162099ebe7f99bc81
adrianojec <adrianojeric.28@gmail.com>
2022-10-23 12:33:02 +0800

    feature: Class, Object, Value and Ref types

---
+++ Employee.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace ems_csharp
+{
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int hoursWorked;
+        public double wage;
+        public double hourlyRate;
+
+        public DateTime birthDay;
+
+        const int minimalHoursWorked = 1;
+
+        public Employee(string firstName, string lastName, string email, DateTime birthDay) : this(firstName, lastName, email, birthDay, 0)
+        {}
+
+        public Employee(string firstName, string lastName, string email, DateTime birthDay, double hourlyRate)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.birthDay = birthDay;
+            this.hourlyRate = hourlyRate;
+        }
+
+        public void PerformWork() => PerformWork(minimalHoursWorked);
+
+        public void PerformWork(int hours)
+        {
+            hoursWorked += hours;
+            Console.WriteLine($"{firstName} {lastName} has worked for {hoursWorked} hour(s)");
+
+        }
+
+        public int CalculateBonus(int bonus)
+        {
+            if (hoursWorked > 10) bonus *= 2;
+            Console.WriteLine($"The employee got a bonus of {bonus}");
+            return bonus;
+        }
+
+        public int CalculateBonusAndBonusTax(int bonus, ref int bonusTax)
+        {
+            if (hoursWorked > 10) bonus *= 2;
+            if(bonus >= 200)
+            {
+                bonusTax = bonus / 10;
+                bonus -= bonusTax;
+            }
+
+            Console.WriteLine($"The employee got a bonus of {bonus} and the tax on the bonus is {bonusTax}");
+
+            return bonus;
+        }
+
+        public double ReceiveWage(bool resetHours = true)
+        {
+            wage = hoursWorked * hourlyRate;
+            Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {hoursWorked} hour(s) of work");
+            if (resetHours) hoursWorked = 0;
+            return wage;
+        }
+
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst Name: \t{firstName}\nLast Name: \t{lastName}" +
+                $"\nEmail: \t\t{email}\nBirthday: \t{birthDay.ToShortDateString()}\n");
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -6,7 +6,88 @@ namespace ems_csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //string userAction;
+            //do
+            //{
+            //    Console.WriteLine("Please choose your action: ");
+            //    Console.WriteLine("1. Add Employee: ");
+            //    Console.WriteLine("2. Update Employee: ");
+            //    Console.WriteLine("3. Delete Employee: ");
+            //    Console.WriteLine("4. View Employees: ");
+            //    Console.WriteLine("0. Quit Console ");
+            //    userAction = Console.ReadLine();
+
+            //    switch (userAction)
+            //    {
+            //        case "1":
+            //            Console.WriteLine("Adding employee ...");
+            //            break;
+            //        case "2":
+            //            Console.WriteLine("Updating employee ...");
+            //            break;
+            //        case "3":
+            //            Console.WriteLine("Deleting employee ...");
+            //            break;
+            //        case "4":
+            //            Console.WriteLine("Loading employees ...");
+            //            break;
+            //        case "0":
+            //            Console.WriteLine("Closing Application ...");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Invalid input!");
+            //            break;
+            //    }
+
+            //} while (userAction != "0");
+
+            //Console.WriteLine("Thank you for using the app.");
+
+            //Console.WriteLine("Creating an Employee");
+            //Console.WriteLine("--------------------");
+
+            Employee john = new Employee("John", "Smith", "johnsmith@gmail.com", new DateTime(1995, 12, 25), 25.0);
+
+            john.PerformWork(50);
+
+            int minimumBonus = 100;
+            //int receivedBonus = john.CalculateBonus(minimumBonus);
+            //Console.WriteLine($"The minimum bonus is {minimumBonus} and {john.firstName} has received" +
+            //    $"bonus of {receivedBonus}");
+
+            int bonusTax = 0;
+            int receivedBonus = john.CalculateBonusAndBonusTax(minimumBonus, ref bonusTax);
+            Console.WriteLine($"The minimum bonus is {minimumBonus}, the bonus tax is {bonusTax} and " +
+                $"{john.firstName} has received a bonus of {receivedBonus}");
+
+            //john.DisplayEmployeeDetails();
+
+            //john.PerformWork();
+            //john.PerformWork();
+            //john.PerformWork();
+            //john.PerformWork();
+            //john.PerformWork();
+
+            //double johnReceivedWage = john.ReceiveWage(true);
+            //Console.WriteLine($"Wage paid (by the program): {johnReceivedWage}");
+
+            //Console.WriteLine("Creating an Employee");
+            //Console.WriteLine("--------------------");
+
+            //Employee emily = new Employee("Emily", "Cruz", "emilycruz@gmail.com", new DateTime(1996, 4, 8), 30.0);
+
+            //john.DisplayEmployeeDetails();
+
+            //emily.PerformWork();
+            //emily.PerformWork();
+            //emily.PerformWork();
+            //emily.PerformWork();
+            //emily.PerformWork();
+
+            //double emilyReceivedWage = emily.ReceiveWage(true);
+            //Console.WriteLine($"Wage paid (by the program): {emilyReceivedWage}");
+
         }
     }
 }
